8dd67a13c5f3b3e4f83edb099628e4c74868537a
PriyanshU Patel <priyanshupatel56@gmail.com>
2023-11-07 18:33:01 +0530

    final

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
+import Lorem from './components/Lorem'
 import ImageSlider from './components/ImageSlider';
 
 
 function App() {
   return (
     <div className="homepage">
-      <div className="left-half">
-   
-      </div>
+    <div className="left-half">
+      <Lorem/>
+    </div>
       <div className="right-half">
       
    <Navbar/>
--- sample/src/components/ImageSlider.js
+++ sample/src/components/ImageSlider.js
@@ -1,39 +1,36 @@
 import React, { useState } from 'react';
-import './ImageSlider.css'
+import './ImageSlider.css'; // Import the CSS file
 
 const ImageSwitcher = () => {
-  const [showSecondImage, setShowSecondImage] = useState(false);
+  const [showFirstImage, setShowFirstImage] = useState(true);
 
   const toggleImage = () => {
-    setShowSecondImage(true);
+    setShowFirstImage(!showFirstImage);
   };
 
   return (
-    <div className="image-switcher">
-      <img
-        src="dog1.png"
-        alt="Image 1"
-        style={{
-          width: '100%',
-          height: '745px',
-          objectFit: 'cover',
-          display: showSecondImage ? 'none' : 'block',
-          cursor: 'pointer',
-        }}
-        onClick={toggleImage}
-      />
-      <img
-        src="dog2.png"
-        alt="Image 2"
-        style={{
-          width: '100%',
-          height: '745px',
-          objectFit: 'cover',
-       
-          display: showSecondImage ? 'block' : 'none',
-        }}
-      />
-    </div>
+    
+      <div className="right-half">
+        <img
+          src="dog1.png"
+          alt="Image 1"
+          style={{
+            display: showFirstImage ? 'block' : 'none',
+            cursor: 'pointer',
+          }}
+          onClick={toggleImage}
+        />
+        <img
+          src="dog2.png"
+          alt="Image 2"
+          style={{
+            display: showFirstImage ? 'none' : 'block',
+            cursor: 'pointer',
+          }}
+          onClick={toggleImage}
+        />
+      </div>
+  
   );
 };
 
